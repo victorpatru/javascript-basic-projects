7b046b60c07de9971006cf131ecf234663145610
Victor Patru <patruvictor.mihai@gmail.com>
2022-06-15 15:31:23 +0300

    finished project 01

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,13 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn")
+const color = document.querySelector(".color")
+
+const getRandomNumber = () => Math.floor(Math.random() * colors.length)
+
+btn.addEventListener("click", () => {
+    const randomNumber = getRandomNumber()
+    // console.log(randomNumber)
+
+    document.body.style.background = colors[randomNumber]
+    color.textContent = colors[randomNumber]
+})
--- 01-color-flipper/setup/hex.js
+++ 01-color-flipper/setup/hex.js
@@ -1,1 +1,24 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+// Need to get the place to put the color
+const color = document.querySelector('.color')
+
+// Add an event listener on the button
+const button = document.getElementById('btn')
+
+// Random Hex generator
+function randomHex() {
+    let hexValue = ['#']
+    for (let i = 0; i < 6; i++) {
+      hexValue.push(hex[Math.floor(Math.random() * hex.length)])
+    }
+    return hexValue.join('')
+}
+
+button.addEventListener('click', () => {
+    const hexValue = randomHex()
+
+    document.body.style.backgroundColor = hexValue
+
+    color.textContent = hexValue
+})
