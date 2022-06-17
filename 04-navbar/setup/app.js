b44359a6eb59cad02a388313027b20db8a49cf3b
Victor Patru <patruvictor.mihai@gmail.com>
2022-06-17 16:29:22 +0300

    exercise 04 solution

--- 04-navbar/setup/app.js
+++ 04-navbar/setup/app.js
@@ -3,3 +3,11 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+
+const navToggle = document.querySelector('.nav_toggle')
+const links = document.querySelector('.links')
+
+navToggle.addEventListener('click', () => {
+    links.classList.toggle('show-links')
+})
