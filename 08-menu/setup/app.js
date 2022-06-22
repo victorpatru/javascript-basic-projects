28a6c9dcc97e47e34974e8dcf291c3897713a935
Victor Patru <patruvictor.mihai@gmail.com>
2022-06-22 17:22:20 +0300

    displaying array item in the DOM dynamically with JS

--- 08-menu/setup/app.js
+++ 08-menu/setup/app.js
@@ -72,3 +72,30 @@ const menu = [
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
 ];
+
+function displayMenuItems(menuItems) {
+  let displayMenu = menuItems.map(item => {
+
+    return `<article class="menu-item">
+              <img src=${item.img} alt=${item.title} class="photo" />
+              <div class="item-info">
+                <header>
+                  <h4>${item.title}</h4>
+                  <h4 class="price">$${item.price}</h4>
+                </header>
+                <p class="item-text">
+                  ${item.desc}
+                </p>
+              </div>
+            </article>`
+  })
+  displayMenu = displayMenu.join('')
+  sectionCenter.innerHTML = displayMenu
+}
+
+const sectionCenter = document.querySelector('.section-center')
+
+window.addEventListener('DOMContentLoaded', () => {
+  displayMenuItems(menu)
+})
+
