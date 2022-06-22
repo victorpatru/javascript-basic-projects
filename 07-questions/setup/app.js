d8cef7a47c8ffb3e471d22eb6b2737fcab19fa81
Victor Patru <patruvictor.mihai@gmail.com>
2022-06-22 14:20:26 +0300

    solutions exercise 07

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,28 @@
 //using selectors inside the element
-// traversing the dom
+const questions = document.querySelectorAll('.question')
+
+questions.forEach(question => {
+    question.addEventListener('click', () => {
+        questions.forEach(item => {
+            if (item !== question) {
+                item.classList.remove('show-text')
+            }
+
+            question.classList.toggle('show-text')
+
+        })
+
+    })
+})
+
+
+// traversing the dom going from question-btn to question-title to the div with the class question
+// const questions = document.querySelectorAll('.question-btn')
+
+// questions.forEach(question => {
+//     question.addEventListener('click', () => {
+//         const questionBox = event.currentTarget.parentElement.parentElement
+
+//         questionBox.classList.toggle('show-text')
+//     })
+// })
